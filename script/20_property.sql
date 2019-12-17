d8387585dbf2aa541a5dd7e52dc21b47aaf56fc1
FUY <david.fuyu@gmail.com>
2019-12-16 22:44:00 -0800

    add insert property generation and generated sql file

--- script/20_property.sql
+++ script/20_property.sql
@@ -16,17 +16,17 @@ CREATE TABLE property (
     property_id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
     property_name VARCHAR(128) NOT NULL, 
     property_desc VARCHAR(1024), 
-    property_type_id INT UNSIGNED NOT NULL, 
     property_category_id INT UNSIGNED NOT NULL, 
+    property_type_id INT UNSIGNED NOT NULL, 
     PRIMARY KEY (property_id),
     FOREIGN KEY(property_type_id) REFERENCES property_type (property_type_id), 
     FOREIGN KEY(property_category_id) REFERENCES property_category (property_category_id) 
 );
 
 INSERT INTO property_category (property_category_name) VALUES 
-('General'), 
+('General characteristics'), 
 ('Enzyme activity'), 
-('C/N sources'), 
+('Growth with different C/N sources'), 
 ('Fermentation');
 
 INSERT INTO property_type (property_type_name) VALUES 
