fcf69930d7c52f02e7aed523469eaf8077d67b38
FUY <david.fuyu@gmail.com>
2020-01-31 21:21:49 -0800

    add verify frontend button

--- app/main/static/src/components/main-organism-property.vue
+++ app/main/static/src/components/main-organism-property.vue
@@ -12,10 +12,8 @@
       <v-btn
         title="Save"
         @click="onClickEditSavelButton"
-        :hidden="!showSaveButton || isVerifying"
+        :hidden="!showEditSavelButton || isVerifying"
       >Save</v-btn>
-
-      <v-divider></v-divider>
 
       <v-btn title="Verify" @click="onClickVerifyButton" :hidden="isVerifying || isEditing">Verify</v-btn>
       <v-btn
@@ -26,7 +24,7 @@
       <v-btn
         title="Confirm"
         @click="onClickVerifyConfirmButton"
-        :hidden="!isVerifying || isEditing"
+        :hidden="!showVerifyConfirmButton || isEditing"
       >Confirm</v-btn>
 
       <div v-if="isEditing">
@@ -41,7 +39,7 @@
                   </v-col>
                   <v-col cols="5">
                     <v-text-field
-                      v-model="formField[p['property_id']]['value']"
+                      v-model="editContent[p['property_id']]['value']"
                       class="text-xs"
                       solo
                       hide-details
@@ -53,6 +51,29 @@
           </div>
         </v-form>
       </div>
+
+      <div v-else-if="isVerifying">
+        <v-form>
+          <div v-for="(c, i) in categories" :key="i">
+            <b style="font-size:1.5em">{{c.property_category_name}}</b>
+            <div v-for="(p,j) in categorizedProperties[c.property_category_name]" :key="j">
+              <!-- <div v-if="verifyContent[p['property_id']]">
+                <b style="margin-left:40px">{{p['property_name']}}</b>
+                {{verifyContent[p['property_id']]['value']}}
+              </div>-->
+              <div v-if="keyed[p['property_id']]">
+                <v-checkbox
+                  style="margin-left:40px"
+                  v-model="verifyContent"
+                  :label="p['property_name']"
+                  :value="keyed[p['property_id']]"
+                ></v-checkbox>
+              </div>
+            </div>
+          </div>
+        </v-form>
+      </div>
+
       <div v-else :hidden="!show">
         <div v-for="(c, i) in categories" :key="i">
           <b style="font-size:1.5em">{{c.property_category_name}}</b>
@@ -76,35 +97,26 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      isEditing: false,
-      isVerifying: false,
       show: false,
       organismId: 0,
       name: "",
-      keyed: {}
+      keyed: {},
+
+      isEditing: false,
+      editContent: {},
+
+      isVerifying: false,
+      verifyContent: []
     };
   },
   computed: {
     ...mapState("category", ["categories"]),
     ...mapState("property", ["properties", "categorizedProperties"]),
-    showSaveButton: function() {
+    showEditSavelButton: function() {
       return this.isEditing;
     },
-    formField: function() {
-      let form = {};
-      for (let i = 0; i < this.properties.length; i++) {
-        let p = this.properties[i]["property_id"];
-        if (this.keyed[p] != null) {
-          form[p] = JSON.parse(JSON.stringify(this.keyed[p]));
-        } else {
-          form[p] = {};
-          form[p]["organism_id"] = this.organismId;
-          form[p]["property_id"] = p;
-          form[p]["value"] = null;
-        }
-      }
-
-      return form;
+    showVerifyConfirmButton: function() {
+      return this.verifyContent.length;
     }
   },
   created() {
@@ -115,8 +127,8 @@ export default {
         let records = response.data.records;
         this.name = records[0]["name"];
         for (let i = 0; i < records.length; i++) {
-          let p = records[i]["property_id"];
-          this.keyed[p] = records[i];
+          let pid = records[i]["property_id"];
+          this.keyed[pid] = records[i];
         }
         this.show = true;
       }
@@ -124,21 +136,34 @@ export default {
   },
   methods: {
     onClickEditButton: function() {
+      for (let i = 0; i < this.properties.length; i++) {
+        let pid = this.properties[i]["property_id"];
+        if (this.keyed[pid] != null) {
+          this.editContent[pid] = JSON.parse(JSON.stringify(this.keyed[pid]));
+        } else {
+          this.editContent[pid] = {};
+          this.editContent[pid]["organism_id"] = this.organismId;
+          this.editContent[pid]["property_id"] = pid;
+          this.editContent[pid]["value"] = null;
+        }
+      }
       this.isEditing = true;
     },
     onClickEditCancelButton: function() {
+      this.editContent = {};
       this.isEditing = false;
     },
     onClickEditSavelButton: function() {
       let todo = {};
       for (let i = 0; i < this.properties.length; i++) {
-        let p = this.properties[i]["property_id"];
+        let pid = this.properties[i]["property_id"];
         if (
-          (this.keyed[p] == undefined && this.formField[p]["value"] != null) ||
-          (this.keyed[p] != undefined &&
-            this.formField[p]["value"] != this.keyed[p]["value"])
+          (this.keyed[pid] == undefined &&
+            this.editContent[pid]["value"] != null) ||
+          (this.keyed[pid] != undefined &&
+            this.editContent[pid]["value"] != this.keyed[pid]["value"])
         ) {
-          todo[p] = JSON.parse(JSON.stringify(this.formField[p]));
+          todo[pid] = JSON.parse(JSON.stringify(this.editContent[pid]));
         }
       }
 
@@ -148,21 +173,24 @@ export default {
           if (response.data.success) {
             let records = response.data.records;
             for (let i = 0; i < records.length; i++) {
-              let p = records[i]["property_id"];
-              this.keyed[p] = records[i];
+              let pid = records[i]["property_id"];
+              this.keyed[pid] = records[i];
             }
           }
           this.isEditing = false;
         });
     },
     onClickVerifyButton: function() {
+      this.verifyContent = [];
       this.isVerifying = true;
     },
     onClickVerifyCancelButton: function() {
+      this.verifyContent = [];
       this.isVerifying = false;
     },
     onClickVerifyConfirmButton: function() {
       this.isVerifying = false;
+      this.verifyContent = [];
     }
   }
 };
