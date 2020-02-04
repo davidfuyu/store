ea866f61bfb2a5f677e1d6b62b3c3979a7b89bcc
FUY <david.fuyu@gmail.com>
2020-02-03 18:41:27 -0800

    add search in app bar

--- app/main/static/src/App.vue
+++ app/main/static/src/App.vue
@@ -2,12 +2,24 @@
   <v-app>
     <navigation :drawer.sync="drawer"></navigation>
 
-    <v-app-bar dense dark app clipped-left fixed>
+    <v-app-bar color="primary" dense dark app clipped-left fixed>
       <v-app-bar-nav-icon @click.stop="drawer= !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title style="width: 400px" class="ml-0 pl-3">
         <span class="title">Information</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <span v-if="organisms.length > 0" class="mt-7">
+        <v-autocomplete
+          :items="organisms"
+          item-text="organism_name"
+          item-value="organism_id"
+          :filter="filterName"
+          v-model="organismId"
+          append-icon="fas fa-search"
+          solo-inverted
+          flat
+        ></v-autocomplete>
+      </span>
       <user-profile></user-profile>
     </v-app-bar>
     <v-content>
@@ -18,8 +30,10 @@
 </template>
 
 <script>
+/* eslint-disable */
 import Navigation from "@/components/navigation.vue";
 import UserProfile from "@/components/user-profile.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "App",
@@ -29,13 +43,30 @@ export default {
   data() {
     return {
       drawer: true,
-      dialog: false
+      dialog: false,
+      organismId: null
     };
+  },
+  computed: {
+    ...mapGetters("organism", { organisms: "organism" })
   },
   beforeCreate() {
     this.$store.dispatch("organism/fetch");
     this.$store.dispatch("property/fetch");
     this.$store.dispatch("category/fetch");
+  },
+  watch: {
+    organismId: function() {
+      this.$router.push(`/organism/${this.organismId}`);
+    }
+  },
+  methods: {
+    filterName(item, queryText, itemText) {
+      const textOne = item.organism_name.toLowerCase();
+      const searchText = queryText.toLowerCase();
+
+      return ( textOne.indexOf(searchText) > -1);
+    }
   }
 };
 </script>
